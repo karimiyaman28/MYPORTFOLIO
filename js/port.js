14742267f5acc2d8f071e3cbecb281a86cb14394
Ramin <Raminkarimi@gmail.com>
2022-05-17 11:24:58 +0430

    update portdetail file

---
+++ js/port.js
@@ -0,0 +1,13 @@
+
+
+
+var nav = document.querySelector("nav");
+window.addEventListener('scroll', function () {
+   if (window.pageYOffset > 100) {
+   	 nav.classList.add("bg-dark");
+
+   } else {
+   	nav.classList.remove('bg-dark');
+   }
+
+});
